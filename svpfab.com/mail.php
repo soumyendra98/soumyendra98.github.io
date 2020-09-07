ea8d7a4dbedb437a1fd0301a21c35ec996458430
soumyendra98 <40710946+soumyendra98@users.noreply.github.com>
2020-09-07 20:28:10 +0530

    Update mail.php

--- svpfab.com/mail.php
+++ svpfab.com/mail.php
@@ -6,7 +6,7 @@
     $targetfile = $targetfolder . basename($_FILES["uploadfile"]["name"]);
     $file_type =strtolower(pathinfo($targetfile,PATHINFO_EXTENSION));
     $allowed = array('iges','dxf','dwg','jpg','jpeg','gif','png','pdf','xlsx','doc','docx');
-    echo $allowed;
+    echo $file_type;
     if (in_array($file_type, $allowed)){
         if(move_uploaded_file($_FILES['uploadfile']['tmp_name'], $targetfile))
         {   
